b7f55cd2121733aefe0067f4c9bcf042b3bf7ed3
Robert DeCaire <robert.decaire@razorfish.ca>
2016-07-15 14:09:15 -0400

    Testing followup

--- src/main/scala/sample/basic/Basic.scala
+++ src/main/scala/sample/basic/Basic.scala
@@ -6,9 +6,9 @@ import akka.routing.RoundRobinPool
 import akka.pattern.ask
 import akka.util.Timeout
 
-import sample.basic.Supervisor.Ping
+import sample.basic.Supervisor.{Followup, Ping}
 import sample.basic.Test1.Message1
-import sample.basic.Test2.Message2
+import sample.basic.Test2.{FatalException, Message2}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -17,24 +17,32 @@ object Test2{
 	def props(system: ActorSystem): Props = Props(classOf[Test2], system)
 
 	case class Message2(s: String)
+  case object Followup2
 	class FatalException extends Exception
 }
 
 class Test2(system: ActorSystem) extends Actor{
-	override def receive: Receive = {
-		case Message2(message) => {
+
+  var messageContainer = "Default"
+
+  override def receive: Receive = {
+		case Message2(message) =>
 			println("Hello from Test2!")
+      messageContainer = message
+      println(s"My message is $messageContainer")
 //			if (math.random < 0.25) {
-				sender() ! s"Got your message ;), it's: $message"
-//			throw new AbsolutelyFatalException()
-		}
-	}
+				//sender() ! s"Got your message ;), it's: $message"
+			throw new FatalException
+    case Test2.Followup2 => println(s"My message is $messageContainer")
+  }
+
 }
 
 object Test1{
 	def props(system: ActorSystem): Props = Props(classOf[Test1], system)
 
 	case class Message1(s: String)
+  case object Followup1
 }
 
 class Test1(system: ActorSystem) extends Actor{
@@ -47,11 +55,16 @@ class Test1(system: ActorSystem) extends Actor{
 		}
 	}
 
+  val child = system.actorOf(Test2.props(system))
+
 	override def receive: Receive = {
-		case Message1(message) => {
+		case Message1(message) =>
 			println("Hello from Test1!")
-			system.actorOf(Test2.props(system)).tell(Test2.Message2(message), sender())
-		}
+			child ! Test2.Message2(message)
+		case Test1.Followup1 =>
+      println("Test 1 following up")
+      child ! Test2.Followup2
+
 	}
 }
 
@@ -59,6 +72,7 @@ object Supervisor {
 	def props(system: ActorSystem): Props = Props(classOf[Supervisor], system)
 
 	case class Ping(s: String)
+  case object Followup
 }
 
 class Supervisor(system: ActorSystem) extends Actor {
@@ -76,8 +90,11 @@ class Supervisor(system: ActorSystem) extends Actor {
 	override def receive: Receive = {
 		case Ping(message) =>
 				println("Hello from Supervisor!")
-//			sender() ! router.tell(Test1.Message1(message), sender())
-				sender() ! s"Got your message ;), it's: $message"
+			  router ! Test1.Message1(message)
+				//sender() ! s"Got your message ;), it's: $message"
+    case Followup =>
+      println("Supervisor following up.")
+      router ! Test1.Followup1
 	}
 }
 
@@ -87,4 +104,6 @@ object Akka extends App {
 
 	implicit val timeout: Timeout = Timeout(1 minute)
 	(actor ? Supervisor.Ping("Akka is Great!")).mapTo[String].map(s => println(s))
+  Thread.sleep(100)
+  actor ! Supervisor.Followup
 }
